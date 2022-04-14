ed30bf5679936c725413adf536a016bb8f8200f5
Kaaveh <Kaaveh@pm.me>
2022-04-14 19:17:03 +0430

    Part four

--- app/src/main/java/ir/kaaveh/calculator/MainActivity.kt
+++ app/src/main/java/ir/kaaveh/calculator/MainActivity.kt
@@ -2,10 +2,27 @@ package ir.kaaveh.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstNumberText = findViewById<EditText>(R.id.editTextNumberOne)
+        val secondNumberText = findViewById<EditText>(R.id.editTextNumberTwo)
+
+        val resultText = findViewById<TextView>(R.id.textResult)
+
+        val addButton = findViewById<Button>(R.id.buttonAdd)
+
+        addButton.setOnClickListener {
+            val firstNumber = firstNumberText.text.toString().toDouble()
+            val secondNumber = secondNumberText.text.toString().toDouble()
+
+            resultText.text = (firstNumber + secondNumber).toString()
+        }
     }
 }
