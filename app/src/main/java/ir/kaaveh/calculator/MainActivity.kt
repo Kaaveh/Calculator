4637b43c72beeeebc746f03252e0b4ab03643c68
Kaaveh <Kaaveh@pm.me>
2022-04-16 08:55:45 +0430

    Part five

--- app/src/main/java/ir/kaaveh/calculator/MainActivity.kt
+++ app/src/main/java/ir/kaaveh/calculator/MainActivity.kt
@@ -7,22 +7,50 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var firstNumber = 0.0
+    private var secondNumber = 0.0
+
+    private lateinit var firstNumberText: EditText
+    private lateinit var secondNumberText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val firstNumberText = findViewById<EditText>(R.id.editTextNumberOne)
-        val secondNumberText = findViewById<EditText>(R.id.editTextNumberTwo)
+        firstNumberText = findViewById(R.id.editTextNumberOne)
+        secondNumberText = findViewById(R.id.editTextNumberTwo)
 
         val resultText = findViewById<TextView>(R.id.textResult)
 
         val addButton = findViewById<Button>(R.id.buttonAdd)
+        val minusButton = findViewById<Button>(R.id.buttonMinus)
+        val multiButton = findViewById<Button>(R.id.buttonMulti)
+        val divideButton = findViewById<Button>(R.id.buttonDivide)
 
         addButton.setOnClickListener {
-            val firstNumber = firstNumberText.text.toString().toDouble()
-            val secondNumber = secondNumberText.text.toString().toDouble()
-
+            getEditText()
             resultText.text = (firstNumber + secondNumber).toString()
         }
+        minusButton.setOnClickListener {
+            getEditText()
+            resultText.text = (firstNumber - secondNumber).toString()
+        }
+        multiButton.setOnClickListener {
+            getEditText()
+            resultText.text = (firstNumber * secondNumber).toString()
+        }
+        divideButton.setOnClickListener {
+            getEditText()
+            resultText.text = (firstNumber / secondNumber).toString()
+        }
+    }
+
+    private fun getEditText() {
+        if (firstNumberText.text.toString().isNotEmpty())
+            firstNumber = firstNumberText.text.toString().toDouble()
+
+        if (secondNumberText.text.toString().isNotEmpty())
+            secondNumber = secondNumberText.text.toString().toDouble()
     }
 }
